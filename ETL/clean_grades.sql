f3420e1d690b6c73c6d73cbbc62e4a1599709711
writezhe <zzhang213@gmail.com>
2016-06-27 22:59:15 +0000

    all fields have been looked at, but course_name, mark, and grade still have at least some oddities

--- ETL/clean_grades.sql
+++ ETL/clean_grades.sql
@@ -1,9 +1,12 @@
--- first, clean the term field --
-select distinct term from clean.all_grades order by term asc;
--- 		build the case statement using the output
-select distinct clean_term, count(clean_term)
-from
-(select term,
+-- **** -- **** --
+-- TERM field -- The fields are either finals, periods 1-4, or semesters
+--		problems: there are some empty fields that are hard to understand
+--			- '' empty string?
+--			- '0' field?
+alter table clean.all_grades add column clean_term varchar(20);
+update only clean.all_grades
+set
+	clean_term =
 	case replace(replace(replace(replace(trim(trailing ' ' from Lower(term)), '9', 'nine'),
 			'weeks', 'week'), 'perod', 'period'), '4tn', '4th')
 	when '' then 'empty'
@@ -34,18 +37,65 @@ from
 	when 'semester 2' then 'sem_2'
 	when 'year' then 'all_year_course'
 	else 'CANNOT FIND CHECK VALUES'
-	end
-	as "clean_term"
-	from clean.all_grades
-) as zzq1 group by clean_term;
+	end;
 
--- next, clean the grade field --
+select distinct clean_term, count(clean_term)
+from clean.all_grades group by clean_term;
 
--- check year field for issues? --
-select distinct year from clean.all_grades;
--- convert years like 2007-08 to 2007
+-- **** -- **** --
+-- GRADE field -- marking what grade the student is in
+--		there are some oddities
+--			- '' (empty). May need to be filled in based on snapshot data
+--			- '**' asterisk.
+--			- '22'. Guess a typo for grade 12?
+--			- '23'. Guess a typo for grade 12?
+--			- 'UG'. Only occurs for one StudentLookup 
+--			- NULL. 
+--		STATUS = wait on these odd cases 
+select distinct grade from clean.all_grades order by grade asc;
+select * from clean.all_grades where grade = '' limit 100;
+select * from clean.all_grades where grade = '**' limit 100;
+select * from clean.all_grades where grade = '22' limit 100;
+select * from clean.all_grades where grade = '23' limit 100;
+select * from clean.all_grades where grade = 'UG' limit 100;
+select * from clean.all_grades where grade is null limit 100;
+
+-- 	Ignoring those oddities above for now.
+--  Instead, we simply just remove the leading zeros from some values
+update only clean.all_grades
+set
+	grade =
+	case
+	when grade = 'KG' then '0'
+	else ltrim(grade, '0')
+	end;
+
+-- output distinct values to assess
+select distinct grade, count(grade)
+from clean.all_grades group by grade order by grade;
+
+-- **** -- **** --
+-- COURSE_NAME & COURSE_CODE -- Lots of variety here
+--		Not a necessary priority currently; can wait until more focused feature creation?
+--		Zhe: I think we just need to parse the course_name, the course_code is too difficult
+select distinct course_name from clean.all_grades order by course_name asc limit 150;
+select distinct course_code from clean.all_grades limit 150;
+
+-- **** -- **** --
+-- MARK field --
+--		If no numbers, treat as a big case statement?
+select distinct mark from clean.all_grades where mark ~ '^[^0-9]*$';
+-- 		If contains only numbers or dots, do a different operation?
+select * from clean.all_grades where mark ~ '^[0-9]*$' limit 100;
+-- 		Zhe: finish the exploration in R
+
+-- **** -- **** --
+-- YEAR field -- Jackie updated to make the years into a single integer value
+-- 		Convert years like 2007-08 to 2007
 alter table clean.all_grades
 	alter column year type int using cast(left(nullif(year, ''),
 				strpos(nullif(year, ''), '-') - 1) as integer) ;
---		we have some 99,455 records with a null year field, why?
+				
+--		Missing? There are 99,455 records with a null year field, why?
+--			We need to see what information we can fill in from other sources
 select year, count(*) from clean.all_grades group by year;
