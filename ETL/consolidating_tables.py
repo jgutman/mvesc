49e697ad8a188aa1699aaf01090eb807ef3ef60b
Hanna <hanna.torrence@gmail.com>
2016-06-27 22:19:29 +0000

    adds query to union snapshots table

--- ETL/consolidating_tables.py
+++ ETL/consolidating_tables.py
@@ -4,8 +4,7 @@ import psycopg2 as pg
 import itertools
 import json
 
-
-##### these should be in a general utility module
+##### these should be in a general utility module once that exists
 def open_db_connection():
     """
     Opens a connection with the mvesc PSQL database
@@ -26,6 +25,19 @@ def open_db_connection():
     connection = pg.connect(host=host_address, database=name_of_database, 
 user=user_name, password=user_password)
     return connection
+
+def get_column_type(cursor, table_name, column_name):
+    """
+    Returns the data type of the given column in the given table
+
+    :param pg cursor: 
+    :param string: table name
+    :param string: column name
+    """
+    my_query = """select data_type from information_schema.columns where table_name = '{0}' and column_name = '{1}';""".format(table_name, column_name)
+    cursor.execute(my_query)
+    column_type = cursor.fetchall()[0][0]
+    return column_type
 
 def get_student_table_names(connection):
     """
@@ -58,6 +70,16 @@ def get_student_table_names(connection):
 
     return table_names
 
+def get_column_names(table, connection):
+    """
+    Get column names of a table
+        
+    :param pg.extensions.connection object connection: sql connection
+    :param string table: table name in the database
+    :rtype: list        
+    """
+    temp_table = pd.read_sql("select * FROM \"%s\" limit 1" % table, connection)
+    return list(temp_table.columns)
 
 ###### specific functions
 
@@ -201,24 +223,56 @@ def all_absences_query():
     from "MATVWMAbsences1516\""""
     return my_query
     
+def all_snapshots_query(snapshot_tables,connection):
+    """
+    Writes a SQL query to drop the current all_snapshots table and create a new one
+    :param pg connection: 
+    :rtype: string                                                                             
+    """
+    # json file with column name and type matchings
+    with open('snapshot_column_names.json', 'r') as f:
+        new_cols_file = json.load(f)
+    new_cols = new_cols_file[u'column_names']
 
-
-
-
+    my_query = "drop table if exists clean.all_snapshots; "
+    my_query +=  """create table clean.all_snapshots as select "StudentLookup\", """
+    for t in snapshot_tables:
+        old_cols = get_column_names(t,connection)
+        #iterating through new table column names
+        for key in sorted(new_cols.keys()):
+            item = new_cols[key]
+            found = 0
+            #select matching column (or null)
+            for c in item[u'name']:
+                if c in old_cols:
+                    if get_column_type(connection.cursor(),t, c) == "character varying":
+                        my_query += "\n cast(nullif(\"" + str(c) + "\", \'\') "
+                        my_query += "as " + str(item[u'type']) + ") "
+                    else:
+                        my_query += "\n cast(\"" + str(c) + "\" "
+                        my_query += "as " + str(item[u'type']) + ") "    
+                    my_query += " as \"" + str(key) + "\","
+                    found = 1
+                    break
+            if found == 0:
+                my_query += "\n cast(null as " + str(item[u'type']) + ")"
+                my_query += " as \"" + str(key) + "\","
+        my_query += " \'20" + t[9:11] + "\' as \"year\" from \"" + t + "\""
+        my_query += " union select \"StudentLookup\", " 
+    my_query = my_query[:-31]+";"
+    return my_query
+    
 ###### script to build tables
 
 connection = open_db_connection()
 table_names = get_student_table_names(connection)
+snapshot_tables = ["Districts{0:02}{1:02}".format(x,x+1) for x in range(6,15)]
 cursor = connection.cursor()
 #cursor.execute(student_lookup_query(table_names))
-cursor.execute(all_grades_query())
+#cursor.execute(all_grades_query())
 #cursor.execute(all_absences_query())
+#cursor.execute(all_snapshots_query(snapshot_tables,connection))
 cursor.close() 
 connection.commit()
 
 
-"""
-with open('snapshot_column_names.json', 'r') as f:
-    new_cols = json.loads(f.read())
-print(new_cols)    
-"""
