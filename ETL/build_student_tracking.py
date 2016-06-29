59553152ee410b4a15f137da454bae9911012f3e
Jacqueline <jacqueline.gutman@gmail.com>
2016-06-29 21:08:04 +0000

    still working on cleaning student tracking functions

--- ETL/build_student_tracking.py
+++ ETL/build_student_tracking.py
@@ -1,6 +1,8 @@
 import psycopg2 as pg
 import numpy as np
 import pandas as pd
+from sqlalchemy import create_engine
+import sqlalchemy
 
 def postgres_pgconnection_generator(pass_file="/mnt/data/mvesc/pgpass"):
     """ Generate a psycopg2 connector
@@ -19,6 +21,25 @@ def postgres_pgconnection_generator(pass_file="/mnt/data/mvesc/pgpass"):
     user_password = passinfo[4]
     connection = pg.connect(host=host_address, database=name_of_database, user=user_name, password=user_password)
     return connection
+
+def postgres_engine_generator(pass_file="/mnt/data/mvesc/pgpass"):
+    """ Generate a sqlalchemy engine to mvesc database
+    Note: you can only run it on the mvesc server
+    :param str pass_file: file with the credential information
+    :return sqlalchemy.engine object engine: object created  by create_engine() in sqlalchemy
+    :rtype sqlalchemy.engine
+    """
+    with open(pass_file, 'r') as f:
+        passinfo = f.read()
+    passinfo = passinfo.strip().split(':')
+    host_address = passinfo[0]
+    port = passinfo[1]
+    user_name = passinfo[2]
+    name_of_database = passinfo[3]
+    user_password = passinfo[4]
+    sql_eng_str = "postgresql://"+user_name+":"+user_password+"@"+host_address+'/'+name_of_database
+    engine = create_engine(sql_eng_str)
+    return engine
 
 def get_all_student_lookups(connection, schema = 'clean',
     table = 'all_snapshots'):
@@ -39,33 +60,44 @@ def get_all_student_lookups(connection, schema = 'clean',
     student_list = [i[0] for i in student_list]
     return student_list
 
-def sql_gen_tracking_students(year_begin, year_end):
-    start_string = """ create view clean.wrk_tracking_students as 
-        select * from clean.all_student_lookups """
+def sql_gen_tracking_students(year_begin, year_end, schema = 'clean',
+    table = 'wrk_tracking_students'):
+
+    start_string = """ create view {}.{} as
+        select * from {}.all_student_lookups """.format(schema, table, schema)
+
     end_string = """ order by "StudentLookup";
-        create table clean.wrk_track_students as
-        (SELECT * FROM clean.wrk_tracking_students LEFT JOIN
-	(SELECT DISTINCT "StudentLookup", withdraw_reason FROM clean.all_snapshots 
-		WHERE withdraw_reason <> 'did not withdraw') AS zzx01 USING ("StudentLookup"));"""
+        create table {}.{} as (SELECT * FROM {}.{} LEFT JOIN
+	(SELECT DISTINCT "StudentLookup", withdraw_reason FROM {}.all_snapshots
+		WHERE withdraw_reason <> 'did not withdraw') AS zzx01 USING ("StudentLookup"));""".format(schema, table, schema, table, schema)
+
     middle_string = """"""
     for yearNum in range(year_begin, year_end+1):
         print(str(yearNum))
         middle_string = middle_string + """
             left join (select distinct "StudentLookup", grade as "{}"
-            from clean.all_snapshots where year = '{}')
-	    as zzq{} using ("StudentLookup")""".format(yearNum, yearNum, yearNum)
-    return start_string + middle_string + end_string
+            from {}.all_snapshots where year = '{}')
+	    as zzq{} using ("StudentLookup")""".format(yearNum, schema, yearNum, yearNum)
 
+    return start_string + middle_string + end_string
 
 def build_wide_format(connection, student_list):
     cursor = connection.cursor()
     get_year_range = """select min(year), max(year) from clean.all_snapshots"""
     cursor.execute(get_year_range)
     min_year, max_year = cursor.fetchone()
+    #cursor.execute(sql_gen_tracking_students(min_year, max_year))
+    min_year = int(min_year)
+    max_year = int(max_year)
+    print(min_year, type(min_year))
+    print(max_year, type(max_year))
 
 def main():
-    connection = postgres_pgconnection_generator()
+    # connection = postgres_pgconnection_generator()
+    connection = postgres_engine_generator().raw_connection()
     all_students = get_all_student_lookups(connection)
+    build_wide_format(connection, all_students)
+    print(sql_gen_tracking_students(2006, 2015))
 
 if __name__ == "__main__":
     main()
