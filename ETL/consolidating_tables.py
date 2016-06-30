3ac15e177c7cd74966e6899fb820a3e5febc4be6
Hanna <hanna.torrence@gmail.com>
2016-06-30 19:42:15 +0000

    making better/more consistent use of connections/cursors and removing lingering capital letters

--- ETL/consolidating_tables.py
+++ ETL/consolidating_tables.py
@@ -3,18 +3,16 @@ import numpy as np
 import psycopg2 as pg
 import itertools
 import json
+from contextlib import contextmanager
 
 ##### these should be in a general utility module once that exists
-def open_db_connection():
+@contextmanager
+def open_db_connection(pass_file):
     """
     Opens a connection with the mvesc PSQL database
 
     :rtype: pg.extensions.connection object connection
     """
-    pass_file = "/mnt/data/mvesc/pgpass" # username, db information
-    with open(pass_file, 'r') as f:
-        passinfo = f.read()
-    passinfo = passinfo.strip().split(':')
     with open(pass_file, 'r') as f:
         passinfo = f.read()
     passinfo = passinfo.strip().split(':')
@@ -22,9 +20,8 @@ def open_db_connection():
     user_name = passinfo[2]
     name_of_database = passinfo[3]
     user_password = passinfo[4]
-    connection = pg.connect(host=host_address, database=name_of_database,
-user=user_name, password=user_password)
-    return connection
+    yield pg.connect(host=host_address, database=name_of_database, user=user_name, password=user_password)
+
 
 def get_column_type(cursor, table_name, column_name):
     """
@@ -32,70 +29,66 @@ def get_column_type(cursor, table_name, column_name):
 
     :param pg cursor:
     :param string: table name
-    :param string: column name
+    :param string: column name'
     """
-    my_query = """select data_type from information_schema.columns where table_name = '{0}' and column_name = '{1}';""".format(table_name, column_name)
-    cursor.execute(my_query)
+    my_query = "select data_type from information_schema.columns where table_name = (%s) and column_name =(%s) ;"
+    cursor.execute(my_query, [table_name, column_name])
     column_type = cursor.fetchall()[0][0]
     return column_type
 
-def get_student_table_names(connection):
+
+def get_column_names(cursor, table, schema="public"):
+    """
+    Get column names of a table
+
+    :param pg object cursor: cursor in psql database 
+    :param string table: table name in the database
+    :rtype: list
+    """
+
+    cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_schema = (%s) and table_name = (%s)", [schema, table])
+    raw_col_names = cursor.fetchall()
+    return list([x[0] for x in raw_col_names])
+
+def get_student_table_names(cursor):
     """
     Retrieves the list of names of tables in the database which are indexed by StudentLookup
 
-    :param pg.extensions.connection object connection: sql connection
+    :param pg object cursor: cursor in psql database
     :rtype: list of strings
     """
 
     my_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-    cursor = connection.cursor()
     cursor.execute(my_query)
     table_names = cursor.fetchall()
     table_names = [t[0] for t in table_names]
-    cursor.close()
-    #filter out tables not indexed by StudentLookup numbers in a more intellegent manner
-    # removing non-student tables
-    table_names.remove('all_lookup')
-    table_names.remove('DistrictSchoolIDs')
-    table_names.remove('DistrictRating1011')
-    table_names.remove('DistrictRating1112')
-    table_names.remove('DistrictRating1213')
-    table_names.remove('DistrictRating1314')
-    table_names.remove('DistrictRating1415')
-
-    # removing replicated tables
-    table_names.remove('OAAOGT')
-    table_names.remove('CurrentMobility')
-    table_names.remove('Mobility_2010_2015')
-
+    to_remove = []
+    for t in table_names:
+        if "StudentLookup" not in get_column_names(cursor,t):
+            to_remove.append(t)
+    for t in to_remove:
+        table_names.remove(t)
     return table_names
 
-def get_column_names(table, connection):
-    """
-    Get column names of a table
-
-    :param pg.extensions.connection object connection: sql connection
-    :param string table: table name in the database
-    :rtype: list
-    """
-    temp_table = pd.read_sql("select * FROM \"%s\" limit 1" % table, connection)
-    return list(temp_table.columns)
 
 ###### specific functions
 
 def student_lookup_query(table_names):
     """
-    Writes a SQL query to drop the current all_student_lookup_table and create a new one
+    Writes a SQL query to drop the current all_student_lookups table and create a new one
     using all the StudentLookup numbers in the given list of tables
 
-    :param list: list of table names
+    :param list table_names: list of table names
     :retype: string
     """
-    my_query = "drop table if exists clean.all_student_lookup; "
-    my_query += "create table clean.all_student_lookup as "
+
+    my_query = "drop table if exists clean.all_student_lookups; "
+    my_query += "create table clean.all_student_lookups as "
     for t in table_names:
-        my_query += "select \"StudentLookup\" from \"" + t + "\" union \n "
-    my_query = my_query[:-6] + ";"
+        my_query += "select \"StudentLookup\" as student_lookup from \"" + t + "\""
+        union_clause = " union \n"
+        my_query += union_clause
+    my_query = my_query[:-len(union_clause)] + ";"
     return my_query
 
 
@@ -105,6 +98,7 @@ def all_grades_query():
 
     :rtype: string
     """
+
     my_query = "drop table if exists clean.all_grades; "
     my_query += "create table clean.all_grades as "
     my_query += """
@@ -114,7 +108,7 @@ def all_grades_query():
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "Schoolyear" as "year",
+    "Schoolyear" as "school_year",
     'Ridgewood' as "district"
     from "Ridgewoodgrades2007_2016"
     union all
@@ -124,17 +118,17 @@ def all_grades_query():
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "Schoolyear" as "year",
+    "Schoolyear" as "school_year",
     'Riverview' as "district"
     from "RiverViewgrades2006_16"
     union all
-    select "StudentLookup",
+    select  "StudentLookup",
     "Course" as "course_code",
     "Cname" as "course_name",
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "Schoolyear" as "year",
+    "Schoolyear" as "school_year",
     'TriValley' as "district"
     from "TriValleyGrades2006_16"
     union all
@@ -144,57 +138,57 @@ def all_grades_query():
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "Schoolyear" as "year",
+    "Schoolyear" as "school_year",
     'West Muskingum' as "district"
     from "WestMuskingumgrades2006_16"
     union all
-    select "StudentLookup",
+    select  "StudentLookup",
     "Course" as "course_code",
     "Cname" as "course_name",
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "Schoolyear" as "year",
+    "Schoolyear" as "school_year",
     'Franklin' as "district"
     from "Franklingrades2006_16"
     union all
-    select "StudentLookup",
+    select  "StudentLookup",
     "Course" as "course_code",
     "Cname" as "course_name",
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "Schoolyear" as "year",
+    "Schoolyear" as "school_year",
     'Maysville' as "district"
     from "Maysvillegrades2006_16"
     union all
-    select "StudentLookup",
+    select  "StudentLookup",
     "Course" as "course_code",
     "Cname" as "course_name",
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "SchoolYear" as "year",
+    "SchoolYear" as "school_year",
     'Coshocton' as "district"
     from "CoshoctonGrades2006_16"
     union all
-    select "StudentLookup",
+    select  "StudentLookup",
     "Course" as "course_code",
     "Cname" as "course_name",
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "SchoolYear" as "year",
+    "SchoolYear" as "school_year",
     'Crooksville' as "district"
     from "CrooksvilleGrades2010_16"
     union all
-    select "StudentLookup",
+    select  "StudentLookup",
     "Course" as "course_code",
     "Cname" as "course_name",
     "term",
     cast("Grade" as text) as "grade",
     "Mark" as "mark",
-    "SchoolYear" as "year",
+    "SchoolYear" as "school_year",
     'East Muskingum' as "district"
     from "EastMuskingumGrades2010_16\"
     """
@@ -209,7 +203,7 @@ def all_absences_query():
     my_query = "drop table if exists clean.all_absences; "
     my_query += "create table clean.all_absences as "
     my_query += """
-    select  "StudentLookup",
+    select  "StudentLookup" as student_lookup,
     "Date" as "date",
     "AbsenceLength" as "absence_length",
     "AbsenceCode" as "absence_code",
@@ -217,7 +211,7 @@ def all_absences_query():
     "School" as "school"
     from "CCFRRWRVabsence09_16"
     union all
-    select  "StudentLookup",
+    select  "StudentLookup" as student_lookup,
     "Date" as "date",
     "AbsenceLength" as "absence_length",
     "AbsenceCode" as "absence_code",
@@ -225,7 +219,7 @@ def all_absences_query():
     "School" as "school"
     from "MATVWMAbsences1415"
     union all
-    select  "StudentLookup",
+    select  "StudentLookup" as student_lookup,
     "Date" as "date",
     "AbsenceLength" as "absence_length",
     "AbsenceCode" as "absence_code",
@@ -235,10 +229,12 @@ def all_absences_query():
     return my_query
 
 
-def all_snapshots_query(snapshot_tables,connection):
+def all_snapshots_query(cursor, snapshot_tables):
     """
     Writes a SQL query to drop the current all_snapshots table and create a new one
-    :param pg connection:
+
+    :param list snapshot_tables: list of tables to include
+    :param pg object cursor: 
     :rtype: string
     """
     # json file with column name and type matchings
@@ -247,9 +243,9 @@ def all_snapshots_query(snapshot_tables,connection):
     new_cols = new_cols_file[u'column_names']
 
     my_query = "drop table if exists clean.all_snapshots; "
-    my_query +=  """create table clean.all_snapshots as select "StudentLookup\", """
+    my_query +=  """create table clean.all_snapshots as select "StudentLookup" as student_lookup, """
     for t in snapshot_tables:
-        old_cols = get_column_names(t,connection)
+        old_cols = get_column_names(cursor,t)
         #iterating through new table column names
         for key in sorted(new_cols.keys()):
             item = new_cols[key]
@@ -257,7 +253,7 @@ def all_snapshots_query(snapshot_tables,connection):
             #select matching column (or null)
             for c in item[u'name']:
                 if c in old_cols:
-                    if get_column_type(connection.cursor(),t, c) == "character varying":
+                    if get_column_type(cursor, t, c) == "character varying":
                         my_query += "\n cast(nullif(\"" + str(c) + "\", \'\') "
                         my_query += "as " + str(item[u'type']) + ") "
                     else:
@@ -269,22 +265,22 @@ def all_snapshots_query(snapshot_tables,connection):
             if found == 0:
                 my_query += "\n cast(null as " + str(item[u'type']) + ")"
                 my_query += " as \"" + str(key) + "\","
-        my_query += " \'20" + t[9:11] + "\' as \"year\" from \"" + t + "\""
-        my_query += " union select \"StudentLookup\", "
-    my_query = my_query[:-31]+";"
+        my_query += " cast( 20" + t[9:11]  + " as int) as school_year from \"" + t + "\""
+        union_clause = " union select \"StudentLookup\" as student_lookup, "
+        my_query += union_clause
+    my_query = my_query[:-len(union_clause)]+";"
     return my_query
 
 ###### script to build tables
-
-connection = open_db_connection()
-table_names = get_student_table_names(connection)
-snapshot_tables = ["Districts{0:02}{1:02}".format(x,x+1) for x in range(6,16)]
-snapshot_tables + ["Districts{0:02}{1:02}_CREM".format(x,x+1) for x in range(10,16)]
-cursor = connection.cursor()
-#cursor.execute(student_lookup_query(table_names))
-#cursor.execute(all_grades_query())
-#cursor.execute(all_absences_query())
-#cursor.execute(all_snapshots_query(snapshot_tables,connection))
-cursor.close()
-connection.commit()
+pass_file = "/mnt/data/mvesc/pgpass" # username, db information
+with open_db_connection(pass_file) as connection:
+    with connection.cursor() as cursor:
+        table_names = get_student_table_names(cursor)
+        snapshot_tables = ["Districts{0:02}{1:02}".format(x,x+1) for x in range(6,16)]
+        snapshot_tables + ["Districts{0:02}{1:02}_CREM".format(x,x+1) for x in range(10,16)]
+        #cursor.execute(student_lookup_query(table_names))
+        #cursor.execute(all_grades_query())
+        #cursor.execute(all_absences_query())
+        #cursor.execute(all_snapshots_query(cursor, snapshot_tables))
+    connection.commit()
 connection.close()
