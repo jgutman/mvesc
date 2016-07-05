2e1ec8f94893f38cf5947c7425ea15c08960b8a8
Jacqueline <jacqueline.gutman@gmail.com>
2016-07-05 15:02:37 +0000

    initial survival analysis numbers only no withdrawal reasons added

--- ETL/build_student_tracking.py
+++ ETL/build_student_tracking.py
@@ -56,16 +56,21 @@ def build_wide_format(cursor, schema = 'clean', snapshots = 'all_snapshots'):
     :param str snapshots: name of table where snapshots table lives
     :return nothing (executes query in database)
     """
-
-     get_year_range = """select min(school_year), max(school_year) from """ \
-            """{}.{}""".format(schema, snapshots)
-     cursor.execute(get_year_range)
-     min_year, max_year = cursor.fetchone()
-     min_year = int(min_year) # these should already be integers anyway
-     max_year = int(max_year)
-     query = sql_gen_tracking_students(min_year, max_year,
+    get_year_range = """
+    select min(school_year), max(school_year) from
+        {schema}.{snapshots}""".format(schema=schema, snapshots=snapshots)
+    cursor.execute(get_year_range)
+    min_year, max_year = cursor.fetchone()
+    min_year = int(min_year) # these should already be integers anyway
+    max_year = int(max_year)
+    query = sql_gen_tracking_students(min_year, max_year,
             schema = schema, snapshots = snapshots)
-     cursor.execute(query)
+    query_survival = cohort_survival_analysis(min_year, max_year, '04',
+        schema = schema)
+    # cursor.execute(query)
+    cursor.execute(query_survival)
+    cohort_results = cursor.fetchall()
+    print(cohort_results)
 
 def sql_gen_tracking_students(year_begin, year_end,
     schema = 'clean', snapshots = 'all_snapshots',
@@ -91,8 +96,8 @@ def sql_gen_tracking_students(year_begin, year_end,
     query_frame = """
     drop table if exists {}.{};
     create table {}.{} as
-		(select * from
-			(select * from (
+        (select * from
+            (select * from (
     """.format(schema, table, schema, table)
 
     for year in range(year_begin, year_end+1):
@@ -110,28 +115,65 @@ def sql_gen_tracking_students(year_begin, year_end,
     query_frame += """ )
     order by student_lookup) as students_grades_only
     left join
-    	(select latest_reason.* from
-    		(select student_lookup, max(district_withdraw_date)
+        (select latest_reason.* from
+            (select student_lookup, max(district_withdraw_date)
             as last_date from
-    			(select distinct student_lookup, withdraw_reason,
+                (select distinct student_lookup, withdraw_reason,
                 district_withdraw_date from {}.{}
-    				where withdraw_reason <> '{}'
+                    where withdraw_reason <> '{}'
                     and withdraw_reason is not null
-    			order by student_lookup, district_withdraw_date) as all_withdraw_reasons
-    		group by student_lookup) as latest_withdrawal
+                order by student_lookup, district_withdraw_date) as all_withdraw_reasons
+            group by student_lookup) as latest_withdrawal
         """.format(schema, snapshots, 'did not withdraw')
 
     query_frame += """
-    	left join
+        left join
             (select distinct student_lookup, withdraw_reason,
             district_withdraw_date from {}.{}) as latest_reason
-    	on latest_withdrawal.student_lookup = latest_reason.student_lookup and
-    	latest_withdrawal.last_date = latest_reason.district_withdraw_date)
+        on latest_withdrawal.student_lookup = latest_reason.student_lookup and
+        latest_withdrawal.last_date = latest_reason.district_withdraw_date)
         as last_withdrawal_only using (student_lookup));
     """.format(schema, snapshots)
 
     # call query as cursor.execute(sql_gen_tracking_students())
     return query_frame
+
+def cohort_survival_analysis(year_begin, year_end, grade_begin,
+    schema = 'clean', table = 'wrk_tracking_students'):
+    """
+    no docstring yet
+    """
+    joined_query = ''
+    next_grade = grade_begin
+    for year in range(year_begin, year_end+1):
+        subquery_get_count = """
+        (select {year} as school_year, count(*) from
+            (select distinct(student_lookup)
+                from {schema}.{table} where "{year_begin}" = '{grade_begin}')
+                as grade_{grade_begin}_in_{year_begin}
+        """.format(schema=schema, table=table, year_begin=year_begin,
+                    grade_begin=grade_begin, year=year)
+        if (year == year_begin):
+            joined_query += """ %s )""" % (subquery_get_count)
+
+        else:
+            next_grade = '%02d' % (int(next_grade)+1)
+            if (next_grade == '13'):
+                break
+
+            subquery_next_year = """
+            where student_lookup in (select distinct(student_lookup)
+                from {schema}.{table} where "{year}" = '{next_grade}'))
+            """.format(schema=schema, table=table, year_begin=year_begin,
+                grade_begin=grade_begin, year=year, next_grade=next_grade)
+
+            joined_query += """
+            union {query_1} {query_2}
+            """.format(query_1 = subquery_get_count,
+            query_2 = subquery_next_year)
+
+    joined_query += """ order by school_year;"""
+    return(joined_query)
 
 def main():
     """
@@ -143,8 +185,8 @@ def main():
     """
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
-            # cursor.execute(query)
             # print(sql_gen_tracking_students(2006, 2015))
+            # print(cohort_survival_analysis(2006, 2015, '04'))
             build_wide_format(cursor)
         connection.commit()
     print('done!')
