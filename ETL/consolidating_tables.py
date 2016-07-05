3dc70f66ef9b9b676cd27c8dbab5432ff3a784b9
Hanna <hanna.torrence@gmail.com>
2016-07-05 22:24:38 +0000

    clarifying snapshots query

--- ETL/consolidating_tables.py
+++ ETL/consolidating_tables.py
@@ -98,6 +98,8 @@ def student_lookup_query(table_names):
     my_query = my_query[:-len(union_clause)] + ";"
     return my_query
 
+# the all_grades and all_absences queries could be updated into the form of the all_snapshots query
+
 
 def all_grades_query():
     """
@@ -250,36 +252,52 @@ def all_snapshots_query(cursor, snapshot_tables):
     with open('snapshot_column_names.json', 'r') as f:
         new_cols_file = json.load(f)
     new_cols = new_cols_file[u'column_names']
-
-    my_query = "drop table if exists clean.all_snapshots; "
-    my_query +=  """create table clean.all_snapshots as """
-    my_query += """select "StudentLookup" as student_lookup, """
+    
+    snapshot_query = """
+    drop table if exists clean.all_snapshots;
+    create table clean.all_snapshots as
+    select "StudentLookup" as student_lookup,
+    """
     for t in snapshot_tables:
         old_cols = get_column_names(cursor,t)
-        #iterating through new table column names
+        #for each new column name
         for key in sorted(new_cols.keys()):
-            item = new_cols[key]
-            found = 0
-            #select matching column (or null)
+            #item contains column type and list of matching old column names
+            item = new_cols[key] 
+            found = 0 
+            #loop through old column names that match the current new one
             for c in item[u'name']:
+                #check if each matching column name exists in the current table
                 if c in old_cols:
+                    #for text, must convert empty string to null before casting
                     if get_column_type(cursor, t, c) == "character varying":
-                        my_query += "\n cast(nullif(\"" + str(c) + "\", \'\') "
-                        my_query += "as " + str(item[u'type']) + ") "
+                        snapshot_query += """
+                        cast(nullif("{old_name}", '') as {type}) as {new_name},
+                        """.format_map({'old_name':str(c), \
+                                        'type':str(item[u'type']), \
+                                        'new_name':str(key)})
                     else:
-                        my_query += "\n cast(\"" + str(c) + "\" "
-                        my_query += "as " + str(item[u'type']) + ") "
-                    my_query += " as \"" + str(key) + "\","
+                        snapshot_query += """
+                        cast("{old_name}" as {type}) as {new_name},
+                        """.format_map({'old_name':str(c), \
+                                        'type':str(item[u'type']),\
+                                        'new_name':str(key)})
                     found = 1
                     break
-            if found == 0:
-                my_query += "\n cast(null as " + str(item[u'type']) + ")"
-                my_query += " as \"" + str(key) + "\","
-        my_query += " cast( 20"+t[9:11]+" as int) as school_year from \""+t+"\""
-        union_clause = " union select \"StudentLookup\" as student_lookup, "
-        my_query += union_clause
-    my_query = my_query[:-len(union_clause)]+";"
-    return my_query
+            if found == 0: #column does not exist in current table
+                snapshot_query += """
+                cast(null as {type}) as {new_name},
+                """.format_map({'new_name':str(key), \
+                                'type':str(item[u'type'])})
+        snapshot_query += """
+        cast(20{year} as int) as school_year from "{table}"
+        """.format_map({'year':t[9:11], 'table':t})
+        union_clause = """ union select "StudentLookup" as student_lookup, """
+        snapshot_query += union_clause
+    snapshot_query = snapshot_query[:-len(union_clause)]+";"
+    return snapshot_query
+
+
 
 ###### script to build tables
 
@@ -289,7 +307,7 @@ with open_db_connection(pass_file) as connection:
         table_names = get_student_table_names(cursor)
         snapshot_tables = ["Districts{0:02}{1:02}".format(x,x+1) \
                            for x in range(6,16)]
-        snapshot_tables + ["Districts{0:02}{1:02}_CREM".format(x,x+1) \
+        snapshot_tables += ["Districts{0:02}{1:02}_CREM".format(x,x+1) \
                            for x in range(10,16)]
         #cursor.execute(student_lookup_query(table_names))
         #cursor.execute(all_grades_query())
