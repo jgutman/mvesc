1cfa54d90e6a988e97ee23c6bc0a77e150ac2aab
Jacqueline <jacqueline.gutman@gmail.com>
2016-07-18 19:09:41 +0000

    add parameter options in define_clfs_params

--- Models_Results/estimate_prediction_model.py
+++ Models_Results/estimate_prediction_model.py
@@ -51,48 +51,42 @@ def df2num(rawdf):
 ######
 # Setup Modeling Options and Functions
 
+# maybe this should be moved to a yaml or json file as well
 def define_clfs_params():
-    clfs = {'logit': LogisticRegression(),
-    'DT': DecisionTreeClassifier()
-    }
-
-    grid = {'logit': {'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]},
-        'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]}
-    }
-    return clfs, grid
-
-# For reference, taken from Rayid's magic loops code
-"""
-def define_clfs_params():
-
-    clfs = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
+    clfs = {
+        'logit': LogisticRegression(),
+        'DT': DecisionTreeClassifier(),
+        'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
         'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
         'AB': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=200),
-        'LR': LogisticRegression(penalty='l1', C=1e5),
-        'logit': LogisticRegression(),
-        'SVM': svm.SVC(kernel='linear', probability=True, random_state=0),
-        'GB': GradientBoostingClassifier(learning_rate=0.05, subsample=0.5,
-        max_depth=6, n_estimators=10),
+        'SVM': svm.SVC(kernel='linear', probability=True),
+        'GB': GradientBoostingClassifier(
+            learning_rate=0.05, subsample=0.5, max_depth=6, n_estimators=10),
         'NB': GaussianNB(),
-        'DT': DecisionTreeClassifier(),
         'SGD': SGDClassifier(loss="hinge", penalty="l2"),
         'KNN': KNeighborsClassifier(n_neighbors=3)
-            }
+    }
 
     grid = {
-    'RF':{'n_estimators': [1,10,100,1000,10000], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
-    'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]},
-    'SGD': { 'loss': ['hinge','log','perceptron'], 'penalty': ['l2','l1','elasticnet']},
-    'ET': { 'n_estimators': [1,10,100,1000,10000], 'criterion' : ['gini', 'entropy'] ,'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
-    'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
-    'GB': {'n_estimators': [1,10,100,1000,10000], 'learning_rate' : [0.001,0.01,0.05,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [1,3,5,10,20,50,100]},
-    'NB' : {},
-    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
-    'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
-    'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
-           }
+        'logit': {'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]},
+        'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100],
+            'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]}
+        'RF':{'n_estimators': [1,10,100,1000,10000], 'max_depth': [1,5,10,20,50,100],
+            'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+        'SGD': {'loss': ['hinge','log','perceptron'], 'penalty': ['l2','l1','elasticnet']},
+        'ET': {'n_estimators': [1,10,100,1000,10000], 'criterion' : ['gini', 'entropy'] ,
+            'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+        'AB': {'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+        'GB': {'n_estimators': [1,10,100,1000,10000], 'learning_rate' : [0.001,0.01,0.05,0.1,0.5],
+            'subsample' : [0.1,0.5,1.0], 'max_depth': [1,3,5,10,20,50,100]},
+        'NB' : {},
+        'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100],
+            'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+        'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+        'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],
+            'algorithm': ['auto','ball_tree','kd_tree']}
+    }
     return clfs, grid
-"""
 
 def clf_loop(clfs, params, train_X, train_y,
         criterion, models_to_run, cv_folds):
