2d9998bb17d35cc5b9c5570cf852036cd8730976
htorrence <hanna.torrence@gmail.com>
2016-07-19 21:33:38 +0000

    updates generate_example to reflect multi-column join function

--- Features/generate_example.py
+++ Features/generate_example.py
@@ -1,22 +1,23 @@
 from feature_utilities import *
 
-def generate_x():
+def generate_x(replace = False):
     schema, table = "model", "x"
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
             create_feature_table(cursor, table)
-
-            # for each feature, each grade level:
             
-                # some code to generate the feature
-                # in column new_col, table new_table
-                # following step will be more efficient
-                # if new_table has an index
-
-                update_column_with_join(cursor, table, column = 'new_col', 
-                                        source_table = 'new_table')
-                # optional parameters:
-                #    source_column - if the source has a different name than desired
-                #    source_schema - if the source is not a temporary table
+            # some code to generate the feature
+            # in column new_col_1, new_col_2, etc in table new_table
+            # following step will be more efficient
+            # if new_table has an index
+            
+            update_column_with_join(cursor, table, 
+                                    column = ['new_col_1', 'new_col_2'], 
+                                    source_table = 'new_table')
+            # optional parameters:
+            #    source_schema - if the source is not a temporary table
+            # note: there is no longer an optional parameter for new column
+            # names - they will be the same in the feature table as 
+            # they are in the table you feed in
 
 
