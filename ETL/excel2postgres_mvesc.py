46aa00b5114e8e1d1839be377c71caccefd8587c
seanxiangcheng <xcheng@mvesc.localdomain>
2016-07-20 15:25:29 +0000

    uploaded miss_transfer_students sent by MVESC on Jul 19

--- ETL/excel2postgres_mvesc.py
+++ ETL/excel2postgres_mvesc.py
@@ -211,3 +211,24 @@ else:
     print("table uploaded to mvesc: ", table_name)
 
 
+#++++++ ~/PartnerData/Sent_to_District_MLF_71916.xlsx ++++++#
+# -1. read Excel file 
+# -2. upload the table
+schema1 = 'raw'
+schema2 = 'public'
+filepath = '/mnt/data/mvesc/PartnerData/Sent_to_District_MLF_71916.xlsx'
+table_name = 'Miss_transfer_MLF_71916'
+print('\n--- processing: ', filepath)
+df = pd.read_excel(filepath, sheetname=0)
+newcol_dict = {col.strip():'_'.join(col.strip().split(' ')).replace('.', '') for col in df.columns}
+df = df.rename(columns=newcol_dict)
+tab_name_json = add_file2table_jsonfile(filepath.split('/')[-1], table_name)
+if tab_name_json==None:
+    print("""Error: File "{}":"{}": table name mapping conflict! Uploading suspended! """.format(filepath.split('/')[-1], table_name))
+else:
+    table_name = df2postgres(df, table_name, nrows=-1, if_exists='replace', schema=schema1)
+    table_name = df2postgres(df, table_name, nrows=-1, if_exists='replace', schema=schema2)
+    print("table uploaded to mvesc: ", table_name)
+
+
+
