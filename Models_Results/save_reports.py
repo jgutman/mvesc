4bb06548596525bf3f8efd18410baa5314dc4113
htorrence <hanna.torrence@gmail.com>
2016-07-21 15:33:30 +0000

    first pass at generating markdown reports

--- Models_Results/save_reports.py
+++ Models_Results/save_reports.py
@@ -25,60 +25,61 @@ def score_distribution(soft_predictions, base_file_name):
     plt.savefig(base_file_name+'_score_dist.png', bbox_inches='tight')
     
 
-def markdown_report(f, model_options, model_name):
-    base_path = '/mnt/data/mvesc/Reports/'
+def markdown_report(f, model_options, base_file_name,  model_name):
     file_name =  model_options['file_save_name']
-    base_file_name = (base_path + file_name +'_' 
-                      + model_name)
+
     # header
-    f.write("# Report for {}".format(" ".join(file_name.split('_')))) # int instead of str error
-    f.write(model_options['user_description'])
-    f.write("Label used: {}".format(model_options['outcome_name']))
+    f.write("# Report for {}\n".format(" ".join(file_name.split('_')))) # int instead of str error
+    f.write(model_options['user_description']+'\n')
+    f.write("Label used: {}\n".format(model_options['outcome_name']))
     
     # model options used
     f.write("### Model Options")
-    f.write("* initial cohort grade: {}"\
+    f.write("* initial cohort grade: {}\n"\
             .format(model_options['cohort_grade_level_begin'][-3:-2]))
-    f.write("* test cohorts: {}"\
-            .format(", ".join(model_options['cohorts_held_out'])))
+    f.write("* test cohorts: {}\n"\
+            .format(", ".join([str(a) for a in 
+                               model_options['cohorts_held_out']])))
     train_set = ", ".join(model_options['cohorts_training'])
     if train_set == "all":
         train_set += " except test/val"
-    f.write("* train cohorts: {}".format(train_set))
+    f.write("* train cohorts: {}\n".format(train_set))
     cv_scheme = " ".join(model_options['model_test_holdout'].split('_'))
     if "fold" in cv_scheme:
         cv_scheme += ", with {} folds".format(model_options['n_folds'])
-    f.write("* cross-validation scheme: {}".format(cv_scheme))
-    f.write("\t * using {}".format(model_options['validation_criterion']))
+    f.write("* cross-validation scheme: {}\n".format(cv_scheme))
+    f.write("\t * using {}\n".format(model_options['validation_criterion']))
     
     
     # features used 
     f.write("### Features Used")
     for key, features in model_options['features_included'].items():
-        f.write("* {}".format(key))
+        f.write("* {}\n".format(key))
         for i in features:
-            f.write("\t * {}".format(i))
+            f.write("\t * {}\n".format(i))
     
     # performance metrics (must have first generated these images)
-    images = [a for a in os.listdir(base_path_name) if 
-              ('png' in a and model_name in a)]
+    report_dir = '.'
+    images = [a for a in os.listdir(report_dir) if 
+              ('png' in a and model_name in a and file_name in a)]
     for fn in images:
-        f.write("![{fn}]({fn}.png)".format(fn=fn))
+        f.write("![{fn}]({fn})\n".format(fn=fn))
         
 def main():
     test_y = np.ones(100)
-    test_set_scores = np.random.randn(100)
-
-    model_options = read_in_yaml()
-    model_name = 'logit'
+    test_set_scores = np.random.rand(100)
+    
     base_path = ''
+    model_options = read_in_yaml("../Models_Results/test_reporting_options.yaml")
+    model_name = 'logit'
     base_file_name = (base_path + model_options['file_save_name'] +'_' 
                       + model_name)
     
     score_distribution(test_set_scores,base_file_name)
 
     with open(base_file_name+'.md','w+') as f:
-        markdown_report(f,model_options,model_name)
+        markdown_report(f,model_options,base_file_name,model_name)
+
 
 
 if __name__ == "__main__":
---
+++ Reports/test_reporting_logit.md
@@ -0,0 +1,20 @@
+# Report for test reporting
+this run is used to test the reporting functions
+Label used: not_on_time
+### Model Options* initial cohort grade: 9
+* test cohorts: 2011
+* train cohorts: a, l, l
+* cross-validation scheme: temporal cohort
+	 * using accuracy
+### Features Used* grades
+	 * gpa_gr_3
+	 * gpa_gr_4
+	 * gpa_gr_5
+	 * gpa_gr_6
+	 * gpa_gr_7
+	 * gpa_gr_8
+	 * gpa_gr_9
+* demographics
+	 * ethnicity
+	 * gender
+![test_reporting_logit_score_dist.png](test_reporting_logit_score_dist.png)
