9690bfb8fefa0733c7f2e5c643cdfe9ad076dfc4
writezhe <zzhang213@gmail.com>
2016-07-22 19:12:26 +0000

    tested and executed generate_snapshot_features.py

--- Features/generate_snapshot_features.py
+++ Features/generate_snapshot_features.py
@@ -1,6 +1,9 @@
-from feature_utilities import *
+import os, sys
+parentdir = os.path.abspath('/home/zzhang/mvesc/ETL')
+sys.path.insert(0,parentdir)
+from feature_utilities import *()
 
-def create_temp_table_of_raw_data_from_snapshots(cursor, grade_range = range(1,10)):
+def create_temp_table_of_raw_data_from_snapshots(connection, cursor, grade_range = range(1,10)):
     """ Contains a manually made list of raw features from the snapshots
     to create a column for each grade level. It collapses the raw data from
     snapshots on student_lookup. The choice of aggregation/collapse is given to
@@ -41,7 +44,7 @@ def create_temp_table_of_raw_data_from_snapshots(cursor, grade_range = range(1,1
     # this initiates a list to store the created column names in the temp table
     list_of_created_grade_specific_columns = []
 
-    print 'Starting to join raw features per grade from snapshots'
+    print('Starting to join raw features per grade from snapshots')
 
     # loop through the given features
     sql_query_individual_columns = 'select student_lookup, '
@@ -60,6 +63,7 @@ def create_temp_table_of_raw_data_from_snapshots(cursor, grade_range = range(1,1
                                           raw_feature = raw_feature,
                                           new_column_name = new_column_name)
 
+        print(raw_feature + ' is finished in temp table.')
         list_of_created_grade_specific_columns.extend(
                 ['{}_gr_{}'.format(raw_feature, x) for x in grade_range])
 
@@ -72,43 +76,37 @@ def create_temp_table_of_raw_data_from_snapshots(cursor, grade_range = range(1,1
         from clean.all_snapshots group by student_lookup);""".\
         format(sql_query_individual_columns = sql_query_individual_columns[:-2]) # the -2 is for the last comma
 
-    cursor.execute(sql_create_temp_table); connection.commit()
+    cursor.execute(sql_create_temp_table)
     sql_create_index = "create index lookup_index on temp_snapshot_table(student_lookup)"
-    cursor.execute(sql_create_index); connection.commit()
-    
-    # get list of created columns in temp table
-    list_of_created_grade_specific_columns = []
+    cursor.execute(sql_create_index)
 
     return list_of_created_grade_specific_columns
 
-def generate_x(replace=False):
+def generate_raw_snapshot_features(replace=False):
     schema, table = "model", "snapshots"
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
             create_feature_table(cursor, table, replace=replace)
             
             # generate temp table for raw snapshot features
-            list_of_temp_cols = create_temp_table_of_raw_data_from_snapshots(cursor)
+            list_of_temp_cols = create_temp_table_of_raw_data_from_snapshots(connection, cursor)
+            
             # merge in with snapshots
             update_column_with_join(cursor, table, 
-                                    column = list_of_temp_cols, 
+                                    column_list = list_of_temp_cols, 
                                     source_table = 'temp_snapshot_table')
-            print 'Finished adding raw features from snapshots'
+            print('Finished adding raw features from snapshots')
 
-            # generate temp table for age-based snapshot features
-            list_of_temp_cols = blank(cursor)
-            # merge in with snapshots
-            update_column_with_join(cursor, table, 
-                                    column = list_of_temp_cols, 
-                                    source_table = 'temp_snapshot_table')
-            print 'Finished adding age-based features from snapshots'
+            connection.commit()
 
-                # some code to generate the feature
-                # in column new_col, table new_table
-                # following step will be more efficient
-                # if new_table has an index
+            # # generate temp table for age-based snapshot features
+            # list_of_temp_cols = blank(cursor)
+            # # merge in with snapshots
+            # update_column_with_join(cursor, table, 
+            #                         column = list_of_temp_cols, 
+            #                         source_table = 'temp_snapshot_table')
+            # print 'Finished adding age-based features from snapshots'
 
-                
-                # optional parameters:
-                #    source_column - if the source has a different name than desired
-                #    source_schema - if the source is not a temporary table
+            # optional parameters:
+            #    source_column - if the source has a different name than desired
+            #    source_schema - if the source is not a temporary table
