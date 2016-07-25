f48d1e6b511710e0704e5e7fe3c866e0a2e3e87a
Jacqueline <jacqueline.gutman@gmail.com>
2016-07-25 15:46:28 +0000

    edit in progress get this script in universal format

--- Features/generate_demographic_features.py
+++ Features/generate_demographic_features.py
@@ -4,13 +4,8 @@ Procedures:
 2. Generate `model.demographics` from `clean.all_snapshots`
 """
 import os, sys
-from os.path import isfile, join, abspath, basename
-from optparse import OptionParser
 import numpy as np
 import pandas as pd
-from sqlalchemy import create_engine
-import sqlalchemy
-import json
 
 pathname = os.path.dirname(sys.argv[0])
 full_pathname = os.path.abspath(pathname)
@@ -18,64 +13,42 @@ split_pathname = full_pathname.split(sep="mvesc")
 base_pathname = os.path.join(split_pathname[0], "mvesc")
 parentdir = os.path.join(base_pathname, "ETL")
 sys.path.insert(0,parentdir)
+
 from mvesc_utility_functions import *
-def update_column_with_join(conn, source_schema, source_table, source_column, 
-                            schema='model', table='demographics', column='ethnicity', dtype='varchar(64)'):
-    """ Update column using join to match another table
-    :param pg.connection conn: pg connection
-    :param str source_schema: schema of source
-    :param str source_table: table of source
-    :param str source_column: column of source
-    :param str schema: schema to update
-    :param str table: table to update
-    :param str column: column to update
-    :param str dtype: data type of new column
-    :return None
-    """
-    with conn.cursor() as cursor:
-        sql_add_column = """alter table {schema}.{table} add column {column} {dtype} default null;""".format(
-            schema=schema, table=table, column=column, dtype=dtype)
-        cursor.execute(sql_add_column); conn.commit()
-        sql_join_cmd = """
-        update {schema}.{table} t1 
-        set {column}=
-        ( 
-            select {source_column} from {source_schema}.{source_table} t2
-            where t2.student_lookup=t1.student_lookup and t2.{source_column} is not null
-            order by {source_column} desc limit 1
-        );
-        """.format(schema=schema, table=table, column=column, 
-        source_schema=source_schema, source_table=source_table, source_column=source_column)
-        cursor.execute(sql_join_cmd); conn.commit()
-        print(""" - updated "{schema}"."{table}"."{col}" from "{s_schema}"."{s_table}"."{s_col}"; """.format(
-        col=column, schema=schema, table=table, s_schema=source_schema, s_table=source_table, s_col=source_column))
-    return None
+from feature_utilities import update_column_with_join
 
-
-
-if __name__=='__main__':
-    schema, table = "model" ,"demographics"
+def main():
+    schema, table = "model" ,"demographics2"
     source_schema, source_table = "clean", "all_snapshots"
+
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
             # drop and create table
             sql_drop = "drop table if exists {schema}.{table};".format(schema=schema, table=table)
-            sql_create="""create table {schema}.{table} as 
+
+            sql_create="""create table {schema}.{table} as
             ( select distinct student_lookup
               from clean.wrk_tracking_students
               where outcome_category is not null
-            );""".format(schema=schema, table=table)           
-            cursor.execute(sql_drop); connection.commit()
-            cursor.execute(sql_create); connection.commit()
+            );""".format(schema=schema, table=table)
+
+            cursor.execute(sql_drop)
+            cursor.execute(sql_create)
+
             sql_create_index = "create index lookup_index on {schema}.{table}(student_lookup)".format(schema=schema, table=table)
             cursor.execute(sql_create_index)
-            connection.commit()
+
             print(""" - Table "{schema}"."{table}" created!""".format(schema=schema, table=table))
 
             # Ethnicity
-            source_schema, source_table, source_column = 'clean', 'all_snapshots', 'ethnicity'
+            source_schema, source_table = 'clean', 'all_snapshots'
+            column_list = ['ethnicity', 'gender']
             column, dtype='ethnicity', 'varchar(64)'
-            update_column_with_join(connection, source_schema, source_table, source_column,
+
+            (cursor, table, column_list, source_table,
+                                        source_schema = None, schema='model')
+
+            update_column_with_join(cursor, table, [], source_schema, source_table, source_column,
                                     schema=schema, table=table, column=column, dtype=dtype)
 
             # Gender
@@ -84,13 +57,13 @@ if __name__=='__main__':
             update_column_with_join(connection, source_schema, source_table, source_column,
                                     schema=schema, table=table, column=column, dtype=dtype)
 
-            
+
             # cleaning due to messy all_snapshots
             sql_cmd = 'update model.demographics set gender=upper(left(trim(gender), 1));'
-            cursor.execute(sql_cmd); 
+            cursor.execute(sql_cmd);
             sql_cmd = """
             UPDATE ONLY model.demographics
-            SET ethnicity = 
+            SET ethnicity =
             case
             when trim(ethnicity)='*' then 'M' --'Multiracial'
             when trim(ethnicity)='1' then 'I' --'American Indian'
@@ -121,5 +94,8 @@ if __name__=='__main__':
             else trim(ethnicity)
             end;
             """
-            cursor.execute(sql_cmd); connection.commit()
+            cursor.execute(sql_cmd)
+        connection.commit()
 
+if __name__=='__main__':
+    main()
