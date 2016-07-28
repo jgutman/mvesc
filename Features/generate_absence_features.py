f2e5167e727fb717f96249680f37e0226ed2250f
seanxiangcheng <xcheng@mvesc.localdomain>
2016-07-28 17:19:08 +0000

    absence feature ready to use

--- Features/generate_absence_features.py
+++ Features/generate_absence_features.py
@@ -23,8 +23,8 @@ sys.path.insert(0,parentdir)
 from mvesc_utility_functions import *
 import numpy as np
 import pandas as pd
+from feature_utilities import *
 
-# functions
 def set_null_as_0(cursor, column, schema='clean', table='absence'):
     """ Set null data points as 0 (be careful to assume so)
 
@@ -38,38 +38,6 @@ def set_null_as_0(cursor, column, schema='clean', table='absence'):
     set {column}=0 
     where {column} is null;""".format(schema=schema, table=table, column=column)
     cursor.execute(sqlcmd)
-
-def create_feature_table(cursor, table, schema = 'model', replace = False):
-    """ The current feature table is dropped and re-created with a single column
-    containing unique student lookups numbers, set as an index
-    :param pg_cursor cursor:
-    :param str table: feature table name 
-    :param str schema: schema name for feature table
-    :param bool replace: if true the table will be replaced, 
-        if false an existing table will not be altered
-    """
-    cursor.execute("""
-    select count(*) from information_schema.tables
-    where table_schema = '{schema}' and table_name = '{table}'
-    """.format_map({'schema':schema,'table':table}))
-    table_exists = cursor.fetchall()[0][0]
-    if (not table_exists) or replace:
-        sql_drop = "drop table if exists {schema}.{table};"\
-            .format(schema=schema, table=table)
-        sql_create = """                                                     
-        create table {schema}.{table} as                                     
-        ( select distinct student_lookup                                     
-        from clean.wrk_tracking_students                                   
-        where outcome_category is not null                                 
-        );""".format(schema=schema, table=table)
-        cursor.execute(sql_drop);
-        cursor.execute(sql_create);
-        sql_create_index = """                                               
-        create index {schema}_{table}_lookup_index on {schema}.{table}(student_lookup)
-        """.format(schema=schema, table=table)
-        cursor.execute(sql_create_index)
-        print(""" - Table {schema}.{table} created!"""\
-        .format(schema=schema, table=table))
 
 def update_column_with_join(cursor, table, column, source_table, 
                             source_column = None, source_schema = 'clean',
@@ -181,7 +149,7 @@ def update_consec(cursor, table, column, source_table,
     :return None:                      
     """
     tab_temp = 'temp_table'
-    source_column = type_str+'_conseq_count'
+    source_column = type_str+'_consec_count'
     sql_drop_temp = """drop table if exists {table};""".format(table=tab_temp)
     cursor.execute(sql_drop_temp)
     sql_create_agg_temp = """
@@ -210,10 +178,9 @@ def update_consec(cursor, table, column, source_table,
     print(""" - updated {schema}.{table}.{col} from {s_schema}.{s_table}.{s_col} for grade {grade}; """.format(
             col=column, schema=schema, table=table, 
             s_schema=source_schema, s_table=source_table, 
-            s_col=source_column, grade=grade))
+            s_col=source_column, grade=grade)
+         )
     
-    
-
 def main():
     schema, table = "model" ,"absence"
     source_schema = "clean"
@@ -234,6 +201,7 @@ def main():
                                 schema='model', grade=grd)
                 set_null_as_0(cursor, column, schema=schema, table=table)
 
+
             # days_absent_unexecused
             source_table, source_column = tab_snapshots, 'days_absent_unexcused'
             new_col_name = 'absence_unexcused'
@@ -244,15 +212,7 @@ def main():
                                 schema='model', grade=grd)
                 set_null_as_0(cursor, column, schema=schema, table=table)
 
-            # discipline_incidents
-#            source_table, source_column = tab_snapshots, 'discipline_incidents'
-#            for grd in range(gr_min, gr_max+1):
-#                column = source_column+'_gr_'+str(grd)
-#                update_column_with_join(cursor, table, column=column, source_table=source_table, 
-#                                source_column = source_column, source_schema = 'clean',
-#                                schema='model', grade=grd)
-#                set_null_as_0(cursor, column, schema=schema, table=table)
-                
+
             # tardy
             source_table = tab_absence
             new_col_name = 'tardy'
@@ -282,8 +242,8 @@ def main():
                                 source_column = None, source_schema = 'clean',
                                 schema='model', type_str='med', grade=grd)
                 set_null_as_0(cursor, column, schema=schema, table=table)
-            
-            # consequtive absence days
+
+            # consecutive absence days
             source_table = tab_absence
             new_col_name = 'absence'
             for grd in range(gr_min, gr_max+1):
@@ -293,7 +253,7 @@ def main():
                             schema='model', type_str = 'absence', grade=grd)
                 set_null_as_0(cursor, column, schema=schema, table=table)
                     
-            # consequtive tardy days
+            # consecutive tardy days
             source_table = tab_absence
             new_col_name = 'tardy'
             for grd in range(gr_min, gr_max+1):
