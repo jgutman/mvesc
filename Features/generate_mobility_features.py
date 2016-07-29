b283aceffcf58b7cd0ecfb00dd489b11738cd32f
Jacqueline <jacqueline.gutman@gmail.com>
2016-07-29 17:01:39 +0000

    intermediate tables built now transform to wide format and join with mobility table

--- Features/generate_mobility_features.py
+++ Features/generate_mobility_features.py
@@ -1,5 +1,14 @@
 from feature_utilities import *
 
+pathname = os.path.dirname(sys.argv[0])
+full_pathname = os.path.abspath(pathname)
+split_pathname = full_pathname.split(sep="mvesc")
+base_pathname = os.path.join(split_pathname[0], "mvesc")
+parentdir = os.path.join(base_pathname, "ETL")
+sys.path.insert(0,parentdir)
+
+from mvesc_utility_functions import *
+
 def create_temp_mobility(cursor, grade_range, table = 'mobility_counts'):
     query_join_mobility_features = """create temporary table {t} as
     select * from
@@ -8,6 +17,7 @@ def create_temp_mobility(cursor, grade_range, table = 'mobility_counts'):
     """.format(t=table)
 
     for max_grade in grade_range:
+        print(max_grade)
         mobility_count_changes = """left join
         (select student_lookup, count(distinct street) n_addresses_to_gr_{gr},
             count(distinct district) n_districts_to_gr_{gr},
@@ -15,32 +25,36 @@ def create_temp_mobility(cursor, grade_range, table = 'mobility_counts'):
             count(school_year) n_records_to_gr_{gr},
             (count(distinct street)-1)/
                 greatest(1, count(street))::float
-                avg_address_change_to_gr_{},
+                avg_address_change_to_gr_{gr},
             (count(distinct district)-1)/
                 greatest(1, count(district))::float
-                avg_district_change_to_gr_{},
+                avg_district_change_to_gr_{gr},
             (count(distinct city)-1)/
                 greatest(1, count(city))::float
-                avg_city_change_to_gr_{}
+                avg_city_change_to_gr_{gr}
         from clean.all_snapshots where grade <= {gr}
         group by student_lookup) mobility_gr_{gr}
         using(student_lookup)
         """.format(gr=max_grade)
         query_join_mobility_features += mobility_count_changes
 
-    # print(query_join_mobility_features)
+    print(query_join_mobility_features)
     cursor.execute(query_join_mobility_features)
     cursor.execute("select * from {t}".format(t=table))
     col_names = [i[0] for i in cursor.description]
     return(col_names[1:])
 
-def generate_x(replace = False):
-    schema, table = "model", "mobility"
+def generate_mobility(replace = False,
+        sql_script = 'mobility_changes_inprogress.sql',
+        schema = 'model', table = 'mobility'):
 
     print("building {}.{}".format(schema, table))
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
-            create_feature_table(cursor, table)
+            if replace:
+                cursor.execute("drop table if exists {}.{}".format(
+                    schema, table))
+                create_feature_table(cursor, table)
 
             # some code to generate the feature
             # in column new_col_1, new_col_2, etc in table new_table
@@ -58,5 +72,14 @@ def generate_x(replace = False):
             # they are in the table you feed in
         connection.commit()
 
+        execute_sql_script(sql_script)
+
+        with postgres_pgconnection_generator() as connection:
+            with connection.cursor() as cursor:
+                # do something
+
+def main():
+    generate_mobility(replace=True)
+
 if __name__ == '__main__':
-    generate_x()
+    main()
--- Features/mobility_changes_inprogress.sql
+++ Features/mobility_changes_inprogress.sql
@@ -9,12 +9,27 @@ $$
 $$ language SQL immutable;
 
 drop table if exists mobility_changes;
+drop table if exists mobility_transitions;
 
-create temporary table mobility_changes as
+create table mobility_changes as
   select distinct on (student_lookup, school_year)
     student_lookup, school_year, grade, status, street, district, city,
     district_withdraw_date, district_admit_date from clean.all_snapshots
   order by student_lookup, school_year,
-    idx(array['active','foster','inactive', 'resident', '%'], status),
-    district_admit_date desc
-;
+    # what are these single digit status codes
+    # priority sort by status : active > foster > inactive > other / null
+    # then for identical statuses, grab the most recent district admit
+    # for binary transition variables only, not for mobility counts
+    idx(array['active','foster','inactive', '%'], status),
+    district_admit_date desc;
+
+create table mobility_transitions as
+select * from
+	(select student_lookup, grade, school_year,
+    street != lag(street) over (partition by student_lookup
+      order by school_year) as different_street,
+    district != lag(district) over (partition by student_lookup
+      order by school_year) as different_district,
+    city != lag(city) over (partition by student_lookup
+      order by school_year) as different_city
+      from mobility_changes) compare_street;
