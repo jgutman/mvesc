2a4e8e961d2d1b5951313332615700273f162735
Jacqueline <jacqueline.gutman@gmail.com>
2016-08-01 16:38:21 +0000

    adding comments and docstrings for clarity

--- Features/generate_mobility_features.py
+++ Features/generate_mobility_features.py
@@ -10,6 +10,9 @@ sys.path.insert(0,parentdir)
 from mvesc_utility_functions import *
 
 def create_temp_mobility(cursor, grade_range, table = 'mobility_counts'):
+    """
+
+    """
     query_join_mobility_features = """create temporary table {t} as
     select * from
         (select distinct(student_lookup)
@@ -42,10 +45,38 @@ def create_temp_mobility(cursor, grade_range, table = 'mobility_counts'):
     col_names = [i[0] for i in cursor.description]
     return(col_names[1:])
 
+def join_mobility_transitions(cursor, grade_range,
+    table = 'mobility_transitions_wide', source_table = 'transition_counts'):
+    """
+
+    """
+    query_join_mobility_features = """create temporary table {t} as
+    select * from
+        (select distinct(student_lookup)
+        from {source_table}) student_list
+    """.format(t=table, source_table=source_table)
+
+    for grade in grade_range:
+        mobility_count_changes = """left join
+        (select student_lookup, num_different_street street_change_to_gr_{gr},
+            num_different_district district_change_to_gr_{gr},
+            num_different_city city_change_to_gr_{gr}
+        from {source_table} where grade = {gr}) mobility_change_gr_{gr}
+        using(student_lookup)
+        """.format(gr=grade, source_table=source_table)
+        query_join_mobility_features += mobility_count_changes
+
+    cursor.execute(query_join_mobility_features)
+    cursor.execute("select * from {t}".format(t=table))
+    col_names = [i[0] for i in cursor.description]
+    return(col_names[1:])
+
 def generate_mobility(replace = False,
         sql_script = 'mobility_changes_inprogress.sql',
         schema = 'model', table = 'mobility'):
+    """
 
+    """
     print("building {}.{}".format(schema, table))
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
@@ -69,12 +100,29 @@ def generate_mobility(replace = False,
             # names - they will be the same in the feature table as
             # they are in the table you feed in
         connection.commit()
+        print('{}.{} updated with mobility_counts'.format(schema, table))
 
-        execute_sql_script(sql_script)
+    execute_sql_script(sql_script)
+    print('temporary tables called from sql script')
 
-        with postgres_pgconnection_generator() as connection:
-            with connection.cursor() as cursor:
-                print('code not finished here')
+    with postgres_pgconnection_generator() as connection:
+        with connection.cursor() as cursor:
+            query_count_transitions = """
+            create temporary table transition_counts as
+            select student_lookup, grade,
+            count(case when different_street then 1 end) num_different_street,
+            count(case when different_district then 1 end) num_different_district,
+            count(case when different_city then 1 end) num_different_city
+            from mobility_transitions
+            group by student_lookup, grade;
+            """
+            cursor.execute(query_count_transitions)
+            column_list = join_mobility_transitions(cursor,
+                grade_range=range(3,13))
+            update_column_with_join(cursor, table, column_list,
+                'mobility_transitions_wide')
+        connection.commit()
+        print('{}.{} updated with mobility_transitions_wide'.format(schema, table))
 
 def main():
     generate_mobility(replace=True)
--- Features/mobility_changes_inprogress.sql
+++ Features/mobility_changes_inprogress.sql
@@ -1,3 +1,7 @@
+-- function to custom sort a list of strings
+-- arg anyarray is an array of strings in preferred sort order
+-- highest priority to lowest priority
+-- anyelement is name of a column where the sort identifier is found
 create or replace function idx(anyarray, anyelement)
   returns int as
 $$
@@ -8,21 +12,29 @@ $$
   limit 1;
 $$ language SQL immutable;
 
+-- replace intermediate tables in public schema
 drop table if exists mobility_changes;
 drop table if exists mobility_transitions;
+
+-- street_clean is the preferred column for student addresses
+-- select one address, district, city row from snapshots per student/school_year
 
 create table mobility_changes as
   select distinct on (student_lookup, school_year)
     student_lookup, school_year, grade, status, street_clean, district, city,
     district_withdraw_date, district_admit_date from clean.all_snapshots
   order by student_lookup, school_year,
-    -- what are these single digit status codes
+    -- what are these single digit status codes?
     -- priority sort by status : active > foster > inactive > other / null
     -- then for identical statuses, grab the most recent district admit
     -- for binary transition variables only, not for mobility counts
     idx(array['active','foster','inactive', '%'], status),
     district_admit_date desc;
 
+-- now that we have one address per student per school_year,
+-- check if address, district, city are the same as previous year
+-- different_street, different_district, different_city are boolean
+-- null if there is no previous year to compare to
 create table mobility_transitions as
 select * from
 	(select student_lookup, grade, school_year,
