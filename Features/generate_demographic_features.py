3d1a19add64881fe8d88a067b9868931e4e9a5f0
Jacqueline <jacqueline.gutman@gmail.com>
2016-08-02 20:25:44 +0000

    fix with up to date demogra

--- Features/generate_demographic_features.py
+++ Features/generate_demographic_features.py
@@ -17,8 +17,25 @@ sys.path.insert(0,parentdir)
 from mvesc_utility_functions import *
 from feature_utilities import update_column_with_join
 
+def create_temp_table(cursor, schema='clean', table='all_snapshots',
+    temp='single_gender', feature='gender'):
+
+    query_rank_feature_by_count = """create temporary table {temp} as
+    (select student_lookup, {feature}, count({feature}),
+        rank() over (
+            partition by student_lookup
+            order by count({feature}) desc)
+    from {schema}.{table}
+    group by student_lookup, {feature});
+    """.format(schema=schema, table=table, temp=temp, feature=feature)
+    query_drop_rows = """delete from {temp}
+    where rank > 1;""".format(temp=temp)
+
+    cursor.execute(query_rank_feature_by_count)
+    cursor.execute(query_drop_rows)
+
 def main():
-    schema, table = "model" ,"demographics2"
+    schema, table = "model" ,"demographics"
     source_schema, source_table = "clean", "all_snapshots"
 
     with postgres_pgconnection_generator() as connection:
@@ -35,67 +52,29 @@ def main():
             cursor.execute(sql_drop)
             cursor.execute(sql_create)
 
-            sql_create_index = "create index lookup_index on {schema}.{table}(student_lookup)".format(schema=schema, table=table)
-            cursor.execute(sql_create_index)
+            print(""" - Table {schema}.{table} created!""".format(schema=schema, table=table))
 
-            print(""" - Table "{schema}"."{table}" created!""".format(schema=schema, table=table))
-
-            # Ethnicity
             source_schema, source_table = 'clean', 'all_snapshots'
-            column_list = ['ethnicity', 'gender']
-            column, dtype='ethnicity', 'varchar(64)'
 
-            (cursor, table, column_list, source_table,
-                                        source_schema = None, schema='model')
+            # add ethnicity - all multiples have already been converted to
+            # M for Multiracial in cleaning_all_snapshots.sql
 
-            update_column_with_join(cursor, table, [], source_schema, source_table, source_column,
-                                    schema=schema, table=table, column=column, dtype=dtype)
+            feature = 'ethnicity'
+            temp_table = 'single_'+feature
+            create_temp_table(cursor, schema=source_schema,
+                table=source_table, temp=temp_table, feature=feature)
+            column_list = [feature]
+            update_column_with_join(cursor, table, column_list, temp_table)
 
-            # Gender
-            source_schema, source_table, source_column = 'clean', 'all_snapshots', 'gender'
-            column, dtype='gender', 'varchar(64)'
-            update_column_with_join(connection, source_schema, source_table, source_column,
-                                    schema=schema, table=table, column=column, dtype=dtype)
+            # add gender - if multiple genders select the most frequent
+            feature = 'gender'
+            temp_table = 'single_'+feature
+            create_temp_table(cursor, schema=source_schema,
+                table=source_table, temp=temp_table, feature=feature)
+            column_list = [feature]
+            update_column_with_join(cursor, table, column_list, temp_table)
 
-
-            # cleaning due to messy all_snapshots
-            sql_cmd = 'update model.demographics set gender=upper(left(trim(gender), 1));'
-            cursor.execute(sql_cmd);
-            sql_cmd = """
-            UPDATE ONLY model.demographics
-            SET ethnicity =
-            case
-            when trim(ethnicity)='*' then 'M' --'Multiracial'
-            when trim(ethnicity)='1' then 'I' --'American Indian'
-            when trim(ethnicity)='2' then 'A' --'Asian/Pacific Islander'
-            when trim(ethnicity)='3' then 'B' --'Black'
-            when trim(ethnicity)='4' then 'H' --'Hispanic'
-            when trim(ethnicity)='5' then 'W' --'White'
-            when trim(ethnicity)='6' then 'M' -- 'Multiracial'
-            when trim(ethnicity)='7' then 'O' --'Other'
-            when lower(trim(ethnicity))='i' then 'I' --'American Indian'
-            when lower(trim(ethnicity))='a' then 'A' --'Asian/Pacific Islander'
-            when lower(trim(ethnicity))='b' then 'B' --'Black'
-            when lower(trim(ethnicity))='h' then 'H' --'Hispanic'
-            when lower(trim(ethnicity))='w' then 'W' --'White'
-            when lower(trim(ethnicity))='m' then 'M' --'Multiracial'
-            when lower(trim(ethnicity))='p' then 'A' --'Asian/Pacific Islander'
-            when lower(trim(ethnicity)) like '%american_ind%' then 'I' -- 'American Indian'
-            when lower(trim(ethnicity)) like '%am_indian%' then 'I' -- 'American Indian'
-            when lower(trim(ethnicity)) like '%hispanic%' then 'H' -- 'Hispanic'
-            when lower(trim(ethnicity)) like '%indian%' then 'I' -- 'Hispanic'
-            when lower(trim(ethnicity)) like '%alaskan%' then 'I' -- 'Hispanic'
-            when lower(trim(ethnicity)) like '%asian%' then 'A' -- 'Asian/Pacific Islander'
-            when lower(trim(ethnicity)) like '%pacific%' then 'A' -- 'Asian/Pacific Islander'
-            when lower(trim(ethnicity)) like '%black%' then 'B' -- 'Black'
-            when lower(trim(ethnicity)) like '%african%' then 'B' -- 'Black'
-            when lower(trim(ethnicity)) like '%multi%' then 'M' -- 'Multiracial'
-            when lower(trim(ethnicity)) like '%white%' then 'W' -- 'White'
-            else trim(ethnicity)
-            end;
-            """
-            cursor.execute(sql_cmd)
-        connection.commit()
+            connection.commit()
 
 if __name__=='__main__':
     main()
