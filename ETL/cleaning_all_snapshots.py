5d83b60fb0c0020f3ba5efd5a20b0acf69836d74
Jacqueline <jacqueline.gutman@gmail.com>
2016-08-03 16:20:11 +0000

    add in Hanna's exact option to clean_column function in branch

--- ETL/cleaning_all_snapshots.py
+++ ETL/cleaning_all_snapshots.py
@@ -7,16 +7,18 @@ def main():
         with connection.cursor() as cursor:
             clean_column(cursor, values="student_status.json",
                          old_column_name="status_code",
-                         table_name="all_snapshots", replace=1)
+                         table_name="all_snapshots", replace=1,
+                         exact=0)
             clean_column(cursor, values="student_status.json",
                          old_column_name="status_desc",
-                         table_name="all_snapshots", replace=1)
-            cursor.execute("""                                                            
-            alter table clean.all_snapshots add column status text;                       
-            alter table clean.all_snapshots alter column status type                      
-            text using coalesce(status_code, status_desc);                                
-            alter table clean.all_snapshots drop column status_code;                      
-            alter table clean.all_snapshots drop column status_desc;                      
+                         table_name="all_snapshots", replace=1,
+                         exact=0)
+            cursor.execute("""
+            alter table clean.all_snapshots add column status text;
+            alter table clean.all_snapshots alter column status type
+            text using coalesce(status_code, status_desc);
+            alter table clean.all_snapshots drop column status_code;
+            alter table clean.all_snapshots drop column status_desc;
             """)
     connection.commit()
     cleaning_grad_grades.main()
