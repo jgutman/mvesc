5f6ad1ebbdfccafb12573a4dd9af809e8028f143
htorrence <hanna.torrence@gmail.com>
2016-08-04 17:20:32 +0000

    tests paths all work with new file structure

--- Models_Results/generate_yaml.py
+++ Models_Results/generate_yaml.py
@@ -1,12 +1,18 @@
-import os
+
+import os, sys
+pathname = os.path.dirname(sys.argv[0])
+full_pathname = os.path.abspath(pathname)
+split_pathname = full_pathname.split(sep="mvesc")
+base_pathname = os.path.join(split_pathname[0], "mvesc")
 import jinja2 as jnj
 import yaml
 import estimate_prediction_model
 
 def generate_yaml(template_options, yaml_location=None):
+    path_name = os.path.join(base_pathname,'Models_Results')
     if not yaml_location:
         yaml_location = 'model_options/'+template_options['batch_name']
-    with(open('all_features.yaml')) as f:
+    with(open(os.path.join(base_pathname,'Features/all_features.yaml'))) as f:
         all_features = yaml.load(f)
 
     if template_options['features'] == 'all':
@@ -20,12 +26,12 @@ def generate_yaml(template_options, yaml_location=None):
                 for f in features['except']:
                     feature_list.remove(f)
                     template_options['features'][table] = feature_list
-                    
-    env = jnj.Environment(loader=jnj.FileSystemLoader('.'))
+    
+    env = jnj.Environment(loader=jnj.FileSystemLoader(path_name))
     env.trim_blocks = True
     temp = env.get_template('model_options.jinja')
-    os.makedirs(yaml_location, exist_ok=True)
-    with(open(os.path.join(yaml_location, 
+    os.makedirs(os.path.join(path_name,yaml_location), exist_ok=True)
+    with(open(os.path.join(path_name, yaml_location, 
                            template_options['name'] + '.yaml'), "w")) as f:
         f.write(temp.render(template_options))
     print(template_options['name'] + '.yaml written!' )
