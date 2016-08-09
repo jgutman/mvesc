5bbc2dc65080282b2c0b63a1dcaaaef6c03f2f0e
Jacqueline <jacqueline.gutman@gmail.com>
2016-08-09 16:02:30 +0000

    update crosstabs function structure

--- Error_Feature_Analysis/feature_cross_tabs.py
+++ Error_Feature_Analysis/feature_cross_tabs.py
@@ -7,12 +7,16 @@ sys.path.insert(0, os.path.join(base_pathname, 'ETL'))
 
 from mvesc_utility_functions import *
 import pandas as pd
+import numpy as np
 import re
 
 def main():
+    optimization_criteria = ['val_precision_5', 'val_recall_5']
+    loop_through_top_models(optimization_criteria)
+
+def loop_through_top_models(optimization_criteria):
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
-            optimization_criteria = ['val_precision_5', 'val_recall_5']
             predictions = None
             top_models_query = """
         create temporary table top_models as
@@ -66,14 +70,39 @@ def main():
                     predictions = predictions.filter(regex=pattern)
                     predictions[['true_label', 'predicted_label']] = \
                     predictions[['true_label', 'predicted_label']].astype(bool)
+                    predictions = make_df_categorical(predictions)
+
+                    crosstabs = build_crosstabs(predictions)
+
                 print('table:{}, features:{}, grades:{}'.format(
                     table_name, feature_tables, feature_grade_range))
                 print(predictions.dtypes)
+
+def build_crosstabs(prediction_data):
+    # do something
+    return crosstabs
 
 def make_df_categorical(raw_data):
     string_features = raw_data.select_dtypes(include=[object, bool])
     numeric_features = raw_data.select_dtypes(include=[np.number])
 
+    for string_col in string_features.columns:
+        raw_data[string_col] = string_features[string_col].astype('category')
+        if len(raw_data[string_col].cat.categories) < 2:
+            raw_data.drop(string_col, axis=1, inplace=True)
+    for numeric_col in numeric_features.columns:
+        num_values = len(numeric_features[numeric_col].unique())
+        num_bins = min(5, num_values)
+        if (num_values < 2):
+            raw_data.drop(numeric_col, axis=1, inplace=True)
+        elif (num_values == 2 and
+            numeric_features[numeric_col].isnull().sum() > 0):
+            raw_data[numeric_col] = numeric_features[numeric_col] \
+                .isnull().astype('category')
+        else:
+            raw_data[numeric_col] = pd.cut(numeric_features[numeric_col],
+                bins = num_bins, precision = 1)
+    return raw_data
 
 if __name__ == '__main__':
     main()
