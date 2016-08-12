2100690af630cbf0bf91052a42cc836fa30631af
htorrence <hanna.torrence@gmail.com>
2016-08-12 16:57:05 +0000

    adding doc string and a 'like' option'

--- Models_Results/generate_yaml.py
+++ Models_Results/generate_yaml.py
@@ -9,6 +9,44 @@ import yaml
 import estimate_prediction_model
 
 def generate_yaml(template_options, yaml_location=None):
+    """
+    This function takes in a dictionary of template options with all the 
+    information necessary to create a model options file.
+    Specifically, this must include:
+    batch_name (str)
+    model_classes (list of str)
+    description (str)
+    name (str)
+    write_to_database (bool)
+    user (str)
+    test_set_type (str)
+    cv_scheme (str)
+    prediction_grade (int)
+    feature_grade_range (list of ints)
+    cohorts_test (list of ints)
+    cohorts_val (list of ints)
+    cohorts_training (list of ints)
+    random_seed (int)
+    cv_criterions (list of str)
+    outcome (str)
+    imputation (str)
+    scaling (str)
+    debug (bool)
+    features (dict or string 'all')
+
+    the features dictionary should have table names as keys, 
+    with each value being either:
+       - the string 'all'
+       - a list of stings which are each either a feature name, a grade-level
+         feature base name with '*' at the end, or a subtring with 
+         'like_' prepended
+       - a dictionary with a single key 'except', with a value 
+         like those above
+
+    :param dict template_options:
+    :param str yaml_location: path to save the yaml file, 
+        defaults to 'model_options/{batch_name}'
+    """
     path_name = os.path.join(base_pathname,'Models_Results')
     if not yaml_location:
         yaml_location = 'model_options/'+template_options['batch_name']
@@ -33,7 +71,7 @@ def generate_yaml(template_options, yaml_location=None):
                                 to_remove.append(f)
                     elif e.split('_')[0] == 'like':
                         # if except starts with 'like', then remove features
-                        # containing the following string following like
+                        # containing the string following like
                         e = '_'.join(e.split('_')[1:])
                         for f in feature_list:
                             if e in f:
@@ -45,7 +83,27 @@ def generate_yaml(template_options, yaml_location=None):
                 for f in to_remove:
                     feature_list.remove(f)
                 template_options['features'][table] = feature_list
-    
+            else: 
+                to_add = []
+                to_remove = []
+                for a in features:
+                    if a.split('_')[0] == 'like': 
+                        # if one of the features had 'like' prepended, 
+                        # then add features containing the string 
+                        #following 'like'
+                        to_remove.append(a)
+                        a  = '_'.join(a.split('_')[1:])
+                        for f in all_features[table]:
+                            if a in f:
+                                to_add.append(f)
+                to_add = set(to_add)
+                to_remove = set(to_remove)
+                for f in to_add:
+                    features.append(f)
+                for f in to_remove:
+                    features.remove(f)
+                template_options['features'][table] = features
+                            
     if type(template_options['cv_criterions']) != list:
         template_options['cv_criterions'] = [template_options['cv_criterions']]
 
@@ -79,11 +137,12 @@ def main():
         'random_seed': 2851,
         'cv_criterions': ['custom_precision_10_20','custom_recall_5_15'],
         'features': {'grades':
-                     {'except':['gpa*']},
+                     {'except':['like__gpa','like_classes']},
                      'demographics': 'all',
                      'mobility': 'all',
                      'oaa_normalized': 
-                     {'except': [8, 'like_pl']}
+                     {'except': [8, 'like_pl', 'like_percentile']},
+                     'absence': ['like_unexcused']
                  },
         'outcome': 'definite',
         'imputation': 'median_plus_dummies',
