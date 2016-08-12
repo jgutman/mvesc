f731176dfe3e19a4c676b27e4ef7775fc2b1bf65
Jacqueline <jacqueline.gutman@gmail.com>
2016-08-12 16:51:16 +0000

    add option parsing

--- Error_Feature_Analysis/copy_select_models_over.py
+++ Error_Feature_Analysis/copy_select_models_over.py
@@ -8,6 +8,7 @@ sys.path.insert(0, os.path.join(base_pathname, 'ETL'))
 from mvesc_utility_functions import *
 import pandas as pd
 import numpy as np
+from optparse import OptionParser
 
 def copy_model_predictions_over(cursor, models_to_copy):
     # copy predictions from predictions schema to model.predictions
@@ -54,10 +55,18 @@ def copy_model_feature_scores_over(cursor, models_to_copy):
             cursor.execute(insert_feature_scores_to_table)
 
 def main():
+    parser = OptionParser()
+    parser.add_option('-m','--model', dest='models_to_copy',
+        action='append', help="model results to copy over to new table")
+    (options, args) = parser.parse_args()
+
     models_to_copy = ['08_09_2016_grade_6_param_set_0_RF_ht_8585',
     '08_09_2016_grade_7_param_set_7_RF_ht_10497',
     '08_09_2016_grade_8_param_set_13_RF_ht_13254',
     '08_09_2016_grade_9_param_set_0_RF_ht_8645',    '08_09_2016_grade_10_param_set_0_RF_ht_8680']
+
+    if options.models_to_copy:
+        models_to_copy = options.models_to_copy
 
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
