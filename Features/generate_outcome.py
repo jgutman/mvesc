2ade021474ae0b2b82bec5c60c5a2a2d6944abd8
writezhe <zzhang213@gmail.com>
2016-08-12 23:03:57 +0000

    moving ogtpass fail to after initial outcome table is made'

--- Features/generate_outcome.py
+++ Features/generate_outcome.py
@@ -13,12 +13,28 @@ sys.path.insert(0,parentdir)
 from mvesc_utility_functions import *
 
 def main():
-    schema, table = "model", "outcome"
+    schema, table = "model", "outcome2"
     source_schema, source_table = "clean", "wrk_tracking_students"
     snapshots = "all_snapshots"
+    current_year = 2016
+    prediction_grade_range = list(range(5,10))
+    prediction_outcome_name = 'future'
 
     with postgres_pgconnection_generator() as connection:
         with connection.cursor() as cursor:
+            insert_outcome_category_predictions = """
+            update {source_schema}.{source_table}
+                set outcome_category = '{predictions}'
+                where student_lookup in
+                (select student_lookup from {source_schema}.{source_table}
+                where "{last_year}" >= {min_grade} and
+                "{last_year}" <= {max_grade} and outcome_category is null)
+            """.format(source_schema=source_schema, source_table=source_table,
+                last_year=current_year-1, predictions = prediction_outcome_name,
+                min_grade = min(prediction_grade_range),
+                max_grade = max(prediction_grade_range))
+            cursor.execute(insert_outcome_category_predictions)
+
             sql_drop_table = """drop table if exists {schema}.{table};""".format(schema=schema, table=table)
             cursor.execute(sql_drop_table)
 
@@ -28,19 +44,31 @@ def main():
             (   select distinct student_lookup,
                 case
                     when outcome_category='on-time' then 0
-                    else 1
+                    when outcome_category not like '{predictions}' then 1
                 end as not_on_time,
                 case
                     when outcome_category='dropout' then 1
-                    else 0
+                    when outcome_category not like '{predictions}' then 0
                 end as is_dropout,
-                case 
+                case
                     when outcome_category='on-time' or outcome_category='late' then 0
                     when outcome_category='dropout' then 1
-                end as definite
+                end as definite,
+                case
+                    when outcome_category = '{predictions}' then 1
+                end as current_students,
+                outcome_bucket,
+                outcome_category
                 from {source_schema}.{source_table}
                 where outcome_category is not null
             ) as all_outcomes
+            left join
+            (   select student_lookup, min(school_year) as cohort_10th
+                from {source_schema}.{snapshots}
+                where grade = 10
+                group by student_lookup
+            ) as cohorts_tenth
+            using(student_lookup)
             left join
             (   select student_lookup, min(school_year) as cohort_9th
                 from {source_schema}.{snapshots}
@@ -49,41 +77,54 @@ def main():
             ) as cohorts_ninth
             using(student_lookup)
             left join
+            (   select student_lookup, min(school_year) as cohort_8th
+                from {source_schema}.{snapshots}
+                where grade = 8
+                group by student_lookup
+            ) as cohorts_eighth
+            using(student_lookup)
+            left join
+            (   select student_lookup, min(school_year) as cohort_7th
+                from {source_schema}.{snapshots}
+                where grade = 7
+                group by student_lookup
+            ) as cohorts_seventh
+            using(student_lookup)
+            left join
             (   select student_lookup, min(school_year) as cohort_6th
                 from {source_schema}.{snapshots}
                 where grade = 6
                 group by student_lookup
             ) as cohorts_sixth
             using(student_lookup)
-
-            left join
-            (   select student_lookup, max(num_ogt_passed) as num_ogt_passed
-                from {schema}.ogt_passfail
-                group by student_lookup
-            ) as num_ogt_passed
-            using(student_lookup)
-
-            left join
-            (   select student_lookup, max(absences_unexcused_gr_11) as absences_unexcused_gr_11,
-                    max(absences_unexcused_gr_12) as absences_unexcused_gr_12
-                from {schema}.absence
-                group by student_lookup
-            ) as absences_unexcused
-            using(student_lookup)            
-
-            order by cohort_9th;
+            order by cohort_10th;
             """.format(schema=schema, table=table,
-            source_schema=source_schema, source_table=source_table,
-            snapshots=snapshots)
+                source_schema=source_schema, source_table=source_table,
+                snapshots=snapshots, predictions=prediction_outcome_name)
             cursor.execute(sql_create_table)
+
+            for grade in prediction_grade_range:
+                update_current_cohorts = """
+                update {schema}.{table}
+                    set cohort_{grade_plus}th = {current_year}
+                    where student_lookup in
+                    (select student_lookup from {source_schema}.{source_table}
+                    where "{last_year}" = {grade})
+                """.format(schema=schema, table=table,
+                    source_schema=source_schema, source_table=source_table,
+                    grade=grade, grade_plus=grade+1,
+                    last_year=current_year-1, current_year=current_year)
+                cursor.execute(update_current_cohorts)
+
             connection.commit()
-            print("""- Table "{schema}"."{table}" created! """.format(
+            print("Table {schema}.{table} created! ".format(
             schema=schema, table=table))
 
             # create index
             sql_create_index = "create index {schema}_{table}_lookup on {schema}.{table}(student_lookup)".format(schema=schema, table=table)
             cursor.execute(sql_create_index); connection.commit()
-            print("""- Index created on {schema}.{table}(student_lookup)!""".format(schema=schema, table=table))
+            print("- Index created on {schema}.{table}(student_lookup)!".format(
+                schema=schema, table=table))
 
 if __name__=='__main__':
     main()
