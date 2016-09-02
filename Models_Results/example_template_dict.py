90f5d69d97c78db0831f5eca43799986000d1761
htorrence <hanna.torrence@gmail.com>
2016-09-02 17:10:23 +0000

    cleaning up Models_Results folder

--- Models_Results/example_template_dict.py
+++ Models_Results/example_template_dict.py
@@ -4,7 +4,8 @@ from generate_yaml import generate_yaml
 # includes all options for each field
 
 template_options = {
-    'batch_name' : 'name_for_folder_with_all_yamls_for_batch'
+    'batch_name' : 'name_for_folder_with_all_yamls_for_batch',
+    'name': 'name_for_specifice_param_set',
     'model_classes': ['logit','DT','RF','ET','AB','SVM','GB','NB','SGD','KNN'],
     'description': 'name_for_this_parameter_set',
     'name': 'yaml_creation',
@@ -13,13 +14,12 @@ template_options = {
     'test_set_type': 'temporal_cohort', # anything other text will result in a random split
     'cv_scheme': 'leave_cohort_out', # k_fold, past_cohorts_only, leave_cohort_out
     # if cv_scheme = 'k_fold', need  'n_folds' key 
-    'prediction_grade': 9, # currently only 9 or 6, soon 6-10
+    'prediction_grade': 9, # between 6 and 10
     'feature_grade_range': range(6,9), # max must be less than prediction_grade
     'cohorts_held_out': [2011,2012], # any range not overlapping with cohorts_training
-    'cohorts_training': [2010]
+    'cohorts_training': [2010],
     'random_seed': 2851,
-    'cv_criterion': 'custom_precision_10', # can replace precision with recall and use any number 0-100 or
-    # f1, average_precision
+    'cv_criterion': 'custom_precision_10', # can replace precision with recall and use any number 0-100 or, can also use f1, average_precision
     # features can be the string 'all', or a dictionary where keys are table names
     # the table names can have a list of features, the string 'all', 
     # or a dictionary with the single key 'except', then a list of features to exclude
@@ -36,13 +36,17 @@ template_options = {
     'debug': True # True or False
 }
 
-time_scales = zip([range(2009,2011),range(2008,2011),range(2007,2011)],
-                  [range(6,9),range(7,9),range(8,9)])
+# optional values:
+# 'subset_n': uses only a small number of students - only for debugging purposes
+# 'downsample_param': if you want to use downsampling, this is the percentage of students in the negative class after rebalancing 
+# 'upsample_param': if you want to use upsampling, this is the percentage of students in the negative class after rebalancing
+# 'sample_wt_ratio: if you want to re-weight classes, re-weighting param passed to sklearn models
+# 'dummify': turn categorical variables into dummy variables (1-hot representation) - defaults to true, true required for some model types
+# 'drop_reference': if 'dummify' is true, this drops the most frequent dummy variable as a reference, to preserve independence of the variables - defaults to true
 
-for years, grades in time_range:
-    template_options['cohorts_training'] = years
-    template_options['feature_grade_range'] = grades
-    template_options['name'] = '{0}_years_data_{1}_cohorts'.\
-                               format(len(grades),len(years))
-    generate_yaml(template_options)
-    estimate_prediction_model.main(['-m',template_options['name']+'.yaml'])
+
+# after making the dictionary, call generate_yaml to build the options file
+generate_yaml(template_options)
+
+# after generating the yaml file, call estimate_prediction_model with the appropriate option
+estimate_prediction_model.main(['-m',template_options['name']+'.yaml'])
